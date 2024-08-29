adfd012042be1ef9532a2001edc73bd48c337bb2
Juvikk <alex.kondor2002@mail.ru>
2024-08-29 19:56:59 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -1,4 +1,10 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append[0]
+        return object.__new__(cls)
+
     def __init__(self, name, floors):
         self.name = name
         self.number_of_floors = floors
@@ -25,6 +31,8 @@ class House:
     def __lt__(self,other):
         if isinstance(self.number_of_floors, int):
             return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
         if isinstance(other.number_of_floors, int) and isinstance(other, House):
@@ -45,6 +53,8 @@ class House:
     def __add__(self, value):
         if isinstance(value, int):
             self.number_of_floors = self.number_of_floors + value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __radd__(self, value):
@@ -54,7 +64,8 @@ class House:
         if isinstance(value, int):
             self.number_of_floors += value
         return self
-
+    def __del__(self):
+        print({self.name}'снесён, но он останется в истории')
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 10)
@@ -80,3 +91,8 @@ print(h1 > h2)
 print(h1 >= h2)
 print(h1 != h2)
 print(h1.__iadd__(8))
+
+
+del h2
+
+print(House.houses_history)
